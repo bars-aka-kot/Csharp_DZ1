80fddef77b106c133c3a0623ba8df6b759b2a60e
BasirovIlgiz <Basirov_ilgiz@mail.ru>
2022-10-02 23:17:46 +0500

    Add Task_008

---
+++ Task_008/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (number > 0)
+{
+    while (i <= number / 2)
+    {
+        Console.Write($"{2 * i} ");
+        i++;
+    }
+}
+else Console.WriteLine("Введенное число отрицательное");
